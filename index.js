097857e3775ff053a758baedbba2cea215c37117
SAhmed28 <sanjida1@ualberta.ca>
2022-10-27 03:46:13 -0600

    CORS installed

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courses = require('./data/courses.json');
 
 app.get('/', (req,res) => {
     res.send('Learning Platform API running');
 })
+
 
 app.listen(port, () => {
     console.log(`Server running on port:`, port);
